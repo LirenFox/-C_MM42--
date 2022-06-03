e251ab35ff40e71e081d03c7b0779d33d9405a64
LirenFox <104010604+LirenFox@users.noreply.github.com>
2022-06-03 18:39:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-// [C_MM42-¤¤] ¨D(-1)^(n+1)*[1/(2n-1)]ªº©M
+// [C_MM42-ä¸­] æ±‚(-1)^(n+1)*[1/(2n-1)]çš„å’Œ
 using namespace std;
 int main(){
     int n;
     double S = 0;
     cin >> n;
-    for(int i=1; i<=n; i++)// 1 - 1/3 + 1/5 - 1/7 + ... + (-1)n+1¡Ñ1/(2n-1)
+    for(int i=1; i<=n; i++)// 1 - 1/3 + 1/5 - 1/7 + ... + (-1)n+1Â¡Ã‘1/(2n-1)
         S = S + pow(-1.0,(i+1)*1.0) * 1/(2*i-1);
     cout << fixed << setprecision(3) << S << endl;
 	return 0;
